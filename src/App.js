8b64b9ff96d1dad669d1dbcacb0a687741e45a94
Warren T. Ferrell <warren.t.ferrell@gmail.com>
2020-10-17 11:52:14 -0600

    bootstrap and basic navbar https://react-bootstrap.github.io/components/navbar/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Warren T. Ferrell Jewell</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">About (soon)</Nav.Link>
+      <Nav.Link href="#link">Blog (soon)</Nav.Link>
+      <NavDropdown title="Projects" id="basic-nav-dropdown">
+      <NavDropdown.Item href="#action/3.1">Computational Donations - coming soon</NavDropdown.Item>
+      <NavDropdown.Item href="#action/3.2">Lights Out - coming soon</NavDropdown.Item>
+      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#link">FAQ</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
   );
 }
 
