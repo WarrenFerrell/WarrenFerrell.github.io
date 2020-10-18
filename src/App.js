1d2be8162049f2edebb2d94c1547cf6d5c460d37
Warren T. Ferrell <warren.t.ferrell@gmail.com>
2020-10-18 12:50:56 -0600

    right justify links

--- src/App.js
+++ src/App.js
@@ -8,22 +8,20 @@ function App() {
   return (
     <div className="App container">
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">Warren T. Ferrell Jewell</Navbar.Brand>
+  <Navbar.Brand href="/">Warren T. Ferrell Jewell</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
+  <Nav className="ml-auto" pullRight="true">
       <Nav.Link href="/About">About (soon)</Nav.Link>
       <Nav.Link href="/Blog">Blog (soon)</Nav.Link>
       <NavDropdown title="Projects" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/projects/musicalscroll">Musical Scroll - coming soon</NavDropdown.Item>
         <NavDropdown.Item href="/projects/bitcoinmining">Computational Donations - coming soon</NavDropdown.Item>
         <NavDropdown.Item href="/projects/lightsout">Lights Out - coming soon</NavDropdown.Item>
       </NavDropdown>
       <Nav.Link href="/faq">FAQ</Nav.Link>
-      <Nav pullRight>
         <Nav.Link href="http://www.linkedin.com/in/warrenferrell">Linkedin</Nav.Link>
       </Nav>
-    </Nav>
   </Navbar.Collapse>
 </Navbar>
 <Routes />
