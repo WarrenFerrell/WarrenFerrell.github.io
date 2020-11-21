79f3f1872380e69059d1f2c5749494734af4b20e
Warren T. Ferrell <warren.t.ferrell@gmail.com>
2020-11-21 14:53:04 -0700

    stash COMMIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from "react";
-import Routes from "./Routes";
-import { Navbar, Nav } from "react-bootstrap";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react';
+import Routes from './Routes';
+import { Navbar, Nav } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App container">
       <Navbar bg="light" expand="lg">
-        <Link to="/" className="navbar-brand">wtfj.dev</Link>
+        <NavLink to="/" className="navbar-brand">wtfj.dev</NavLink>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto" pullRight="true">
-            <NavLink to="/home" className="nav-link">Home</NavLink>
-            <NavLink to="/faq" className="nav-link">About</NavLink>
+            <NavLink to="/faq" className="nav-link">About/FAQ</NavLink>
             <NavLink to="/blog" className="nav-link">Blog (soon)</NavLink>
             <NavLink to="/projects" className="nav-link">Projects</NavLink>
-            <NavLink to="/faq" className="nav-link">FAQ</NavLink>
-            <Nav.Link href="http://www.linkedin.com/in/warrenferrell">Linkedin</Nav.Link>
+            <Navbar.Brand href="http://www.github.com/warrenferrell" className="navbar-brand"><img 
+              src="/GitHub-Mark-32px.png" 
+              className="d-inline-block align-center"
+              alt="Github logo"/></Navbar.Brand>
+            <Navbar.Brand href="http://www.linkedin.com/in/warrenferrell" className="navbar-brand"><img 
+              src="/LI-In-Bug-32.png" 
+              className="d-inline-block align-center"
+              alt="LinkedIn logo"/></Navbar.Brand>
           </Nav>
+          
         </Navbar.Collapse>
       </Navbar>
       <Routes />
---
+++ src/containers/Delete.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Tab, Tabs, Navbar, Nav } from "react-bootstrap";
+import { NavLink, Switch, Route } from "react-router-dom";
+import NotFound from "./NotFound";
+
+export default class extends Component {
+  render() {
+    const path = "/projects";
+    return (
+      <div>
+        <div className="links">
+          <Navbar bg="light" expand="lg">
+            <NavLink
+              to={`${path}/musicScroll`}
+              className="nav-link"
+            >
+              MusicScroll.js
+            </NavLink>
+            <NavLink
+              to={`${path}/bitcoin`}
+              className="nav-link"
+            >
+              Bitcoin Mining
+            </NavLink>
+            <NavLink
+              to={`${path}/lightsout`}
+              className="nav-link"
+            >
+              Neural Lights Out
+            </NavLink>
+          </Navbar>
+        </div>
+		<div className="tabs">
+		<Switch>
+		  <Route path={`${path}/musicScroll`}  component={NotFound}/>
+		  <Route path={`${path}/bitcoin`}  component={NotFound}/>
+		  <Route path={`${path}/lightsout`}  component={NotFound}/>
+		</Switch>
+		</div>
+      </div>
+    );
+  }
+}
